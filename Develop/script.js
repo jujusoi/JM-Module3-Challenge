64181be6f12a069452cc0f756cfe61010f6dfd28
jujusoi <jalxmcdonald@hotmail.com>
2023-07-03 13:13:22 +0930

    Finished logic, pass will only generate characters that are selected

--- Develop/script.js
+++ Develop/script.js
@@ -11,11 +11,14 @@ console.log(capLetters);
 
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+var wantLowercase = false;
+var wantUppercase = false;
+var wantNumbers = false;
+var wantSpecialChars = false;
+
 function writePassword() {
  if (window.confirm("Do you want to write a new password?")) { prompts();
   function prompts() { var passWordLength = window.prompt("Input a password length between 8 and 128.");
-
    {
     if (passWordLength < 8) {
       window.alert("Password cannot be less than 8 characters.");
@@ -26,61 +29,113 @@ function writePassword() {
     } else { lowercase();
       function lowercase() {
       if (window.confirm("Do you want lowercase characters in your password?")) {
-        console.log("i want lwoercase stuff")
-        var pass1 = letters;
-        console.log(pass1);
+        console.log("i want lowercase")
+        wantLowercase = true;
         uppercase();
+        return wantLowercase;
       } else {
+        console.log("i dont want lowercase")
+        wantLowercase = false;
         uppercase();
+        return wantLowercase;
       }
       function uppercase() {
         if (window.confirm("Do you want uppercase characters in your password?")) {
-
           console.log("i want uppercase")
-          var pass2 = capLetters;
-          console.log(pass2);
+          wantUppercase = true;
           getNumbers();
+          return wantUppercase;
         } else {
+          console.log("i dont want uppercase")
+          wantUppercase = false;
           getNumbers();
+          return wantUppercase;
         }
       }
       function getNumbers() {
         if (window.confirm("Do you want numbers in your password?")) {
-          var pass3 = numbers;
-          console.log(pass3);
           console.log("i want numbers")
+          wantNumbers = true;
           iWantSpecialCharacters();
+          return wantNumbers;
         } else {
+          console.log("i dont want numbers")
+          wantNumbers = false;
           iWantSpecialCharacters();
+          return wantNumbers;
         }
       }
       function iWantSpecialCharacters() {
         if (window.confirm("Do you want special characters in your password?")) {
-          var pass4 = specialCharacters;
-          console.log(pass4);
           console.log("i want special characters")
+          wantSpecialChars = true;
           generatePasswordAll();
+          return wantSpecialChars
         } else {
-          return
+          console.log("i dont want special characters")
+          wantSpecialChars = false;
+          generatePasswordAll();
+          return wantSpecialChars;
         }
       }
       function generatePasswordAll() {
-        var newPassword = letters.concat(numbers, specialCharacters, capLetters, numbers, specialCharacters, capLetters, letters);
-        console.log(newPassword);
-        var shuffledPassword = newPassword.sort((a, b) => 0.5 - Math.random());
-        console.log(shuffledPassword);
-        var spliceValue = shuffledPassword.length - passWordLength
-        var finalPassword = shuffledPassword.splice(spliceValue);
-        console.log(finalPassword);
-        var stringPass = finalPassword.toString();
-        var splitComma = stringPass.replaceAll(",", "");
-        console.log(splitComma);
-        displayPassword(splitComma);
+        console.log(wantLowercase);
+        console.log(wantUppercase);
+        console.log(wantNumbers);
+        console.log(wantSpecialChars);
+        var newPassword = "";
+        if (wantLowercase === false && wantUppercase === false && wantNumbers === false && wantSpecialChars === false) {
+          newPassword = "";
+        } else if (wantLowercase === false && wantUppercase && wantNumbers && wantSpecialChars) {
+          newPassword = capLetters.concat(numbers, specialCharacters, capLetters, numbers, specialCharacters, numbers, capLetters);
+        } else if (wantLowercase && wantUppercase === false && wantNumbers && wantSpecialChars) {
+          newPassword = letters.concat(numbers, specialCharacters, letters, numbers, specialCharacters, numbers, letters);
+        } else if (wantLowercase && wantUppercase && wantNumbers === false && wantSpecialChars) {
+          newPassword = letters.concat(specialCharacters, specialCharacters, capLetters, letters, specialCharacters, letters, capLetters);
+        } else if (wantLowercase && wantUppercase && wantNumbers && wantSpecialChars === false) {
+          newPassword = letters.concat(numbers, numbers, capLetters, numbers, letters, numbers, capLetters);
+        } else if (wantLowercase === false && wantUppercase == false && wantNumbers && wantSpecialChars) {
+          newPassword = numbers.concat(numbers, specialCharacters, specialCharacters, numbers, specialCharacters, numbers, specialCharacters);
+        } else if (wantLowercase === false && wantUppercase && wantNumbers === false && wantSpecialChars) {
+          newPassword = capLetters.concat(capLetters, specialCharacters, capLetters, capLetters, specialCharacters, capLetters, specialCharacters);
+        } else if (wantLowercase === false && wantUppercase && wantNumbers && wantSpecialChars === false) {
+          newPassword = capLetters.concat(numbers, numbers, numbers, numbers, capLetters, numbers, capLetters);
+        } else if (wantLowercase && wantUppercase === false && wantNumbers === false && wantSpecialChars) {
+          newPassword = letters.concat(specialCharacters, specialCharacters, letters, letters, specialCharacters, letters, letters);
+        } else if (wantLowercase && wantUppercase === false && wantNumbers && wantSpecialChars === false) {
+          newPassword = letters.concat(numbers, numbers, numbers, numbers, numbers, letters, letters);
+        } else if (wantLowercase && wantUppercase && wantNumbers === false && wantSpecialChars === false) {
+          newPassword = letters.concat(capLetters, capLetters, letters, letters, capLetters, letters, letters);
+        } else if (wantLowercase === false && wantUppercase === false && wantNumbers === false && wantSpecialChars) {
+          newPassword = specialCharacters.concat(specialCharacters, specialCharacters, specialCharacters, specialCharacters, specialCharacters, specialCharacters, specialCharacters);
+        } else if (wantLowercase === false && wantUppercase === false && wantNumbers && wantSpecialChars === false) {
+          newPassword = numbers.concat(numbers, numbers, numbers, numbers, numbers, numbers, numbers);
+        } else if (wantLowercase === false && wantUppercase && wantNumbers === false && wantSpecialChars === false) {
+          newPassword = capLetters.concat(capLetters, capLetters, capLetters, capLetters, capLetters);
+        } else if (wantLowercase && wantUppercase === false && wantNumbers === false && wantSpecialChars === false) {
+          newPassword = letters.concat(letters, letters, letters, letters, letters);
+        } else {
+          newPassword = letters.concat(capLetters, letters, specialCharacters, numbers, capLetters, numbers, specialCharacters, numbers, numbers, specialCharacters, letters);
+        }
+        makePassFromLogic(newPassword);
       }
+
+function makePassFromLogic(newPassword) {
+  var shuffledPassword = newPassword.sort(() => 0.5 - Math.random());
+  console.log(shuffledPassword);
+  var spliceValue = shuffledPassword.length - passWordLength
+  var finalPassword = shuffledPassword.splice(spliceValue);
+  console.log(finalPassword);
+  var stringPass = finalPassword.toString();
+  var splitComma = stringPass.replaceAll(",", "");
+  console.log(splitComma);
+  displayPassword(splitComma);
+}
+
       function displayPassword(examplePass) {
         var textArea = document.body.querySelector("#password");
         console.log(textArea);
-        textArea.textContent = examplePass;
+        textArea.value = examplePass;
       }
       }
     }
@@ -96,15 +151,4 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-/* for (i = 0; i < passWordLength; i++) {
-  passwordArray.push(randomLetter(letters));
-}
-console.log(passwordArray); 
-
-          for (i = 0; i < passWordLength; i++) {
-            passwordArray.push(randomCaps(capLetters));
-          }
-          console.log(passwordArray);*/
